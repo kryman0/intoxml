33b518c06e51d17fd9b25d3db6b906962f8b065c
kryman0 <kryman0@gmail.com>
2026-08-23 16:34:01 +0200

    Filtering and creates XML files by year

--- IntoXml/Program.cs
+++ IntoXml/Program.cs
@@ -1,5 +1,4 @@
-﻿using IntoXml;
-using IntoXml.Sql;
+﻿using IntoXml.Sql;
 using IntoXml.Xml;
 
 var data = MockData.ReadData().Where(x => x.PatientPnr == "191001010101");
--- IntoXml/Xml/XmlBuilder.cs
+++ IntoXml/Xml/XmlBuilder.cs
@@ -7,26 +7,34 @@ public static class XmlBuilder
 {
     public static void BuildXml(IEnumerable<AuditLog> auditLogs)
     {
-        var filename = Path.Combine(Environment.CurrentDirectory, "Patient.xml");
         var patient = auditLogs.FirstOrDefault(x => x.PatientPnr == "191001010101");
-
+        
         var patientElement = new XElement("Patient",
             new XElement("Namn", patient.PatientFirstname + " " + patient.PatientLastname),
             new XElement("Pnr", patient.PatientPnr));
-        
-        var doc = new XDocument(
-            new XElement("PlaneradeÅtgärder", 
-                new XElement(patientElement),
-                from al in auditLogs
-                select
-                    new XElement("Användare",
-                        new XElement("Namn", al.UserFirstname + " " + al.UserLastname),
-                        new XElement("Pnr", al.UserPnr),
-                        new XElement("Tidpunkt", al.LogDate),
-                        al.Insats != null ? XElement.Parse(al.Insats) : null
-                    )
+
+        foreach (var logsByYear in auditLogs.GroupBy(x => x.LogDate.Year))
+        {
+            var doc = new XDocument(
+                new XElement("PlaneradeÅtgärder",
+                    new XElement(patientElement)
                 )
-        );
-        doc.Save(filename);
+            );
+            
+            foreach (var log in logsByYear)
+            {
+                var user = new XElement("Användare",
+                    new XElement("Namn", log.UserFirstname + " " + log.UserLastname),
+                    new XElement("Pnr", log.UserPnr),
+                    new XElement("Tidpunkt", log.LogDate),
+                    log.Insats != null ? XElement.Parse(log.Insats) : null);
+                doc.Root.Add(user);
+            }
+
+            var filename = Path.Combine(Environment.CurrentDirectory,
+                $"Signeringslista_{patient.PatientFirstname}_{patient.PatientLastname}_{logsByYear.Key}.xml");
+            
+            doc.Save(filename);
+        }
     }
 }
